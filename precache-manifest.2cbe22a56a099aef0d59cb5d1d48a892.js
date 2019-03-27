c40ce4e2b0ef5c1d125a14cad2a93019e4ae8f67
Julian Prucca <julianprucca@Julians-MacBook-Pro.local>
2019-03-27 19:26:50 -0300

    v3 release

---
+++ precache-manifest.2cbe22a56a099aef0d59cb5d1d48a892.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "12e99e683ae89fa96797",
+    "url": "/js/chunk-vendors.5bc08a64.js"
+  },
+  {
+    "revision": "b0e701277f7f62451433",
+    "url": "/js/app.19142f45.js"
+  },
+  {
+    "revision": "12d2b0f52b16f82711067400b3264477",
+    "url": "/index.html"
+  },
+  {
+    "revision": "555b326fd9f20fbfa8ef0edb6cb962ab",
+    "url": "/img/background.555b326f.png"
+  },
+  {
+    "revision": "b0e701277f7f62451433",
+    "url": "/css/app.ac9f1855.css"
+  }
+];
